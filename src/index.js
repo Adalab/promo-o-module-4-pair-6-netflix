8c0e991e11718499016b7f265c4a714e91263986
SaraSanchezL <sarasanchezlopez93@gmail.com>
2022-02-03 11:34:04 +0100

    add statics servers, exercises 1-3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const movies = require ('./data/movies.json')
+const express = require("express");
+const cors = require("cors");
+const movies = require("./data/movies.json");
+const users = require("./data/users.json");
 
 // create and config server
 const server = express();
@@ -13,12 +14,40 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+//servidor de estáticos
+const staticServerPath = "./web/public";
+server.use(express.static(staticServerPath));
+const staticServerPathImg = "./web/src/public-movies-images/";
+server.use(express.static(staticServerPathImg));
+
 //escribimos los endpoints
 server.get("/movies", (req, res) => {
   const response = {
     success: true,
-    movies: movies
+    movies: movies,
   };
   res.json(response);
-}
-);
+});
+
+server.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  //const userId =  users.map((user) => user.id);
+
+  if (
+    users.find((user) => user.email === email && user.password === password)
+  ) {
+    console.log("Inicio de sesión correcto");
+    //console.log(userId);
+    res.json({
+      success: true,
+      userId: email,
+    });
+  } else {
+    res.status(404);
+    res.json({
+      success: false,
+      errorMessage: "Usuaria/o no encontrada/o",
+    });
+  }
+});
