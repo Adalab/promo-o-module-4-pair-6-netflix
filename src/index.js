30c1fd892421fcc52f7821ddb77c9f6aa7230feb
SaraSanchezL <sarasanchezlopez93@gmail.com>
2022-02-08 11:44:57 +0100

    add exercises 4.6 1-4 and start exercise 5

--- src/index.js
+++ src/index.js
@@ -8,28 +8,30 @@ const Database = require("better-sqlite3");
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.set('view engine', 'ejs');
+server.set("view engine", "ejs");
 
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-//servidor de estáticos para las imágenes 
+//servidor de estáticos para las imágenes
 const staticServerPathImg = "./web/src/public-movies-images/";
 server.use(express.static(staticServerPathImg));
 const staticServerPathStyles = "./web/src/stylesheets/";
 server.use(express.static(staticServerPathStyles));
 
-// Base de datos 
-const db = new Database('./src/db/database.db', {verbose: console.log});
+// Base de datos
+const db = new Database("./src/db/database.db", { verbose: console.log });
 
 //escribimos los endpoints
 server.get("/movies", (req, res) => {
   let movies = [];
-  const query = db.prepare('SELECT * FROM movies WHERE gender= ? ORDER BY title DESC ');
-  const queryAll = db.prepare('SELECT * FROM movies ORDER BY title DESC ');
-  if (req.query.gender === ''){
+  const query = db.prepare(
+    "SELECT * FROM movies WHERE gender= ? ORDER BY title DESC "
+  );
+  const queryAll = db.prepare("SELECT * FROM movies ORDER BY title DESC ");
+  if (req.query.gender === "") {
     movies = queryAll.all();
   } else {
     movies = query.all(req.query.gender);
@@ -37,7 +39,7 @@ server.get("/movies", (req, res) => {
   const response = {
     success: true,
     movies: movies,
-  }; 
+  };
   res.json(response);
 });
 
@@ -64,16 +66,65 @@ server.post("/login", (req, res) => {
   }
 });
 
-server.get('/movie/:movieId', (req, res) => { 
-  const urlParams =req.params.movieId;
-  const foundMovie = movies.find( movie => movie.id === urlParams) 
-  if (!foundMovie.title){
+server.get("/movie/:movieId", (req, res) => {
+  const urlParams = req.params.movieId;
+  const foundMovie = movies.find((movie) => movie.id === urlParams);
+  if (!foundMovie.title) {
     foundMovie.title = "";
   }
   // no hace falta else porque si existe, nos devuelve el valor que toca
   console.log(foundMovie);
-  res.render('movie', foundMovie);
+  res.render("movie", foundMovie);
 });
+
+server.post("/sign-up", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const querySelectUser = db.prepare("SELECT * FROM users WHERE email=?");
+  const foundUser = querySelectUser.get(email);
+
+  if (foundUser === undefined) {
+    const query = db.prepare(
+      "INSERT INTO users (email, password) VALUES (?,?)"
+    );
+    const newUser = query.run(email, password);
+    res.json({
+      success: true,
+      userId: newUser.lastInsertRowid,
+    });
+  } else {
+    res.json({
+      error: true,
+      errorMessage: "el usuario ya existe",
+    });
+  }
+});
+
+
+// REVISAR PARA COMPLETAR EJERCICIO 5, 4.6
+server.post("/profile", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userId = req.headers('user-id');
+
+const query =  db.prepare("UPDATE users SET email=?, password=? WHERE userId=?");
+const userUpdate = query.run(email,password);
+
+if(userUpdate.changes !== 0) {
+  res.json({
+    error: false,
+    message: "modificado con exito"
+  })
+} else {
+  res.json ({
+    error: true, 
+    message: "ha ocurrido un error"
+  })
+}
+  
+})
+
 //servidor de estáticos
 const staticServerPath = "./web/public";
 server.use(express.static(staticServerPath));
--- web/src/services/api-user.js
+++ web/src/services/api-user.js
@@ -22,16 +22,16 @@ const sendSingUpToApi = (data) => {
   console.log("Se están enviando datos al signup:", data);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   return fetch(
-    "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json"
+    "http://localhost:4000/sign-up", {
+      method: "POST",
+      headers : {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    }
   )
-    .then((response) => response.json())
-    .then(() => {
-      // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: false,
-        errorMessage: "Usuario ya existente",
-      };
-    });
+  .then(response => response.json())
+  .then(data => {
+     return data;
+  });
 };
 
 // profile
@@ -40,8 +40,16 @@ const sendProfileToApi = (userId, data) => {
   console.log("Se están enviando datos al profile:", userId, data);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   return fetch(
-    "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json"
-  );
+    "http://localhost:4000/profile", {
+    method: "POST", 
+    headers : {"user-id": userId},
+    body: JSON.stringify(data)
+    }
+  )
+  .then(response => response.json())
+  .then(data => {
+   return data;
+});
 };
 
 const getProfileFromApi = (userId) => {
